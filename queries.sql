a92eaf40a9bcdb0b6834c0f987adf6dffbd06f27
Camryn Scott <camryn1242@yahoo.com>
2023-06-15 19:57:33 -0400

    Finished third query

--- queries.sql
+++ queries.sql
@@ -10,4 +10,12 @@ FROM owners o
 JOIN vehicles v 
 ON o.id = v.owner_id 
 GROUP BY first_name, last_name 
-ORDER BY first_name  
+ORDER BY first_name
+--third
+SELECT first_name, last_name, ROUND(AVG(price)) as average_price
+FROM owners o
+JOIN vehicles v
+ON o.id = v.owner_id
+GROUP BY first_name, last_name
+HAVING COUNT(owner_id) > 1 AND ROUND(AVG(price)) > 10000
+ORDER BY first_name DESC;
