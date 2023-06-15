ae2245684b9a5521e9a0b1c8da43e194b9aba3d9
Camryn Scott <camryn1242@yahoo.com>
2023-06-15 19:43:10 -0400

    finished second query and re structered the code pattern

--- queries.sql
+++ queries.sql
@@ -1,5 +1,13 @@
 -- write your queries here
 --first
-SELECT * FROM owners o FULL JOIN vehicles v ON o.id = v.owner_id;
+SELECT * 
+FROM owners o 
+FULL JOIN vehicles v 
+ON o.id = v.owner_id;
 --second
-  
+SELECT first_name, last_name, COUNT(owner_id) 
+FROM owners o 
+JOIN vehicles v 
+ON o.id = v.owner_id 
+GROUP BY first_name, last_name 
+ORDER BY first_name  
